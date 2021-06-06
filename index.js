e3a9bf1d2f435e927d9a18c08b4ab8bea3430316
Souvik Sarkhel <souviksarkhel.kgp@gmail.com>
2021-06-06 17:26:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+document.getElementById("btn").addEventListener("click", function(){
+  var n = document.getElementById('nooftimes').value;
+  var start = document.getElementById('rangestart').value;
+  var end = document.getElementById('rangeend').value;
+  var range=end-start;
+  if(start>end){alert("Please Enter The Range Correctly");}
+  var ans="";
+  for(var i=0;i<n;i++)
+  {
+    ans+=Math.floor((Math.random() * range) + start)+" ";
+  }
+  document.getElementById("amplified").innerHTML=ans;
+});
